106888cf7db0ae6d3fc9684617ae3e5f70103c66
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-02 18:03:43 +0530

    Build the function

--- app/src/main/java/com/naveen/sendingdatafromfragmenttoactivity/MainActivity.kt
+++ app/src/main/java/com/naveen/sendingdatafromfragmenttoactivity/MainActivity.kt
@@ -2,10 +2,34 @@ package com.naveen.sendingdatafromfragmenttoactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name : TextView
+    lateinit var email : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.textViewName)
+        email = findViewById(R.id.textViewEmail)
+
+        val fm : FragmentManager = supportFragmentManager
+        val ft : FragmentTransaction = fm.beginTransaction()
+        val myFragment = MyFragment()
+        ft.add(R.id.frame, myFragment)
+        ft.commit()
+
+    }
+
+    fun getData(userName : String, userEmail: String){
+
+        name.text = userName
+        email.text = userEmail
+
     }
 }
--- app/src/main/java/com/naveen/sendingdatafromfragmenttoactivity/MyFragment.kt
+++ app/src/main/java/com/naveen/sendingdatafromfragmenttoactivity/MyFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 class MyFragment : Fragment() {
+
+    lateinit var name : EditText
+    lateinit var email : EditText
+    lateinit var submit : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view : View = inflater.inflate(R.layout.fragment_my, container, false)
+
+        name = view.findViewById(R.id.editTextName)
+        email = view.findViewById(R.id.editTextEmail)
+        submit = view.findViewById(R.id.buttonSubmit)
+
+        submit.setOnClickListener {
+
+            val userName = name.text.toString()
+            val userEmail = email.text.toString()
+
+            (activity as MainActivity).getData(userName,userEmail)
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my, container, false)
+        return view
     }
 }
